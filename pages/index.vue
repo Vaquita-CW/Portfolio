a42e0886a36003d8b55aa4a565806523ae00e67b
Vaquita-CW <shift_@tutanota.com>
2022-11-14 11:33:47 +0100

    second commit

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,28 @@
       About Page
     </NLink>
   </div>
+  <Renderer ref="renderer">
+    <Camera :position="{ z: 10 }" />
+    <Scene>
+      <PointLight :position="{ y: 50, z: 50 }" />
+      <Box ref="box" :rotation="{ y: Math.PI / 4, z: Math.PI / 4 }">
+        <LambertMaterial />
+      </Box>
+    </Scene>
+  </Renderer>
 </template>
 
 <script>
 export default {
   head: {
     title: 'Home page'
-  }
+  },
+  mounted() {
+    const renderer = this.$refs.renderer;
+    const box = this.$refs.box.mesh;
+    renderer.onBeforeRender(() => {
+      box.rotation.x += 0.01;
+    });
+  },
 }
 </script>
